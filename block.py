0b74fe8b746b8fef9fc0b77c3a860b5b1f2502bd
eamon <42105040+yimingliu123@users.noreply.github.com>
2020-11-26 22:26:30 +0800

    Add files via upload

---
+++ block.py
@@ -0,0 +1,261 @@
+import torch.nn as nn
+
+import torch
+import torch.nn.functional as F
+import math
+
+def conv_layer(in_channels, out_channels, kernel_size, stride=1, dilation=1, groups=1):
+    padding = int((kernel_size - 1) / 2) * dilation
+    return nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=padding, bias=True, dilation=dilation,
+                     groups=groups)
+
+
+
+class OctaveConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, alpha_in=0.5, alpha_out=0.5, stride=1, padding=0, dilation=1,
+                 groups=1, bias=False):
+        super(OctaveConv, self).__init__()
+        self.downsample = nn.AvgPool2d(kernel_size=(2, 2), stride=2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        assert stride == 1 or stride == 2, "Stride should be 1 or 2."
+        self.stride = stride
+        self.is_dw = groups == in_channels
+        assert 0 <= alpha_in <= 1 and 0 <= alpha_out <= 1, "Alphas should be in the interval from 0 to 1."
+        self.alpha_in, self.alpha_out = alpha_in, alpha_out
+        self.conv_l2l = None if alpha_in == 0 or alpha_out == 0 else \
+                        nn.Conv2d(int(alpha_in * in_channels), int(alpha_out * out_channels),
+                                  kernel_size, 1, padding, dilation, math.ceil(alpha_in * groups), bias)
+        self.conv_l2h = None if alpha_in == 0 or alpha_out == 1 or self.is_dw else \
+                        nn.Conv2d(int(alpha_in * in_channels), out_channels - int(alpha_out * out_channels),
+                                  kernel_size, 1, padding, dilation, groups, bias)
+        self.conv_h2l = None if alpha_in == 1 or alpha_out == 0 or self.is_dw else \
+                        nn.Conv2d(in_channels - int(alpha_in * in_channels), int(alpha_out * out_channels),
+                                  kernel_size, 1, padding, dilation, groups, bias)
+        self.conv_h2h = None if alpha_in == 1 or alpha_out == 1 else \
+                        nn.Conv2d(in_channels - int(alpha_in * in_channels), out_channels - int(alpha_out * out_channels),
+                                  kernel_size, 1, padding, dilation, math.ceil(groups - alpha_in * groups), bias)
+
+    def forward(self, x):
+        x_h, x_l = x if type(x) is tuple else (x, None)
+
+        x_h = self.downsample(x_h) if self.stride == 2 else x_h
+        x_h2h = self.conv_h2h(x_h)
+        x_h2l = self.conv_h2l(self.downsample(x_h)) if self.alpha_out > 0 and not self.is_dw else None
+        if x_l is not None:
+            x_l2l = self.downsample(x_l) if self.stride == 2 else x_l
+            x_l2l = self.conv_l2l(x_l2l) if self.alpha_out > 0 else None
+            if self.is_dw:
+                return x_h2h, x_l2l
+            else:
+                x_l2h = self.conv_l2h(x_l)
+                x_l2h = self.upsample(x_l2h) if self.stride == 1 else x_l2h
+                x_h = x_l2h + x_h2h
+                x_l = x_h2l + x_l2l if x_h2l is not None and x_l2l is not None else None
+                return x_h, x_l
+        else:
+            return x_h2h, x_h2l
+
+class Conv_BN(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, alpha_in=0.5, alpha_out=0.5, stride=1, padding=0, dilation=1,
+                 groups=1, bias=False, norm_layer=nn.BatchNorm2d):
+        super(Conv_BN, self).__init__()
+        self.conv = OctaveConv(in_channels, out_channels, kernel_size, alpha_in, alpha_out, stride, padding, dilation,
+                               groups, bias)
+        self.bn_h = None if alpha_out == 1 else norm_layer(int(out_channels * (1 - alpha_out)))
+        self.bn_l = None if alpha_out == 0 else norm_layer(int(out_channels * alpha_out))
+
+    def forward(self, x):
+        x_h, x_l = self.conv(x)
+        x_h = self.bn_h(x_h)
+        x_l = self.bn_l(x_l) if x_l is not None else None
+        return x_h, x_l
+
+
+class Conv_BN_ACT(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, alpha_in=0.5, alpha_out=0.5, stride=1, padding=0, dilation=1,
+                 groups=1, bias=False, norm_layer=nn.BatchNorm2d, activation_layer=nn.ReLU):
+        super(Conv_BN_ACT, self).__init__()
+        self.conv = OctaveConv(in_channels, out_channels, kernel_size, alpha_in, alpha_out, stride, padding, dilation,
+                               groups, bias)
+        self.bn_h = None if alpha_out == 1 else norm_layer(int(out_channels * (1 - alpha_out)))
+        self.bn_l = None if alpha_out == 0 else norm_layer(int(out_channels * alpha_out))
+        self.act = activation_layer(inplace=True)
+
+    def forward(self, x):
+        x_h, x_l = self.conv(x)
+        x_h = self.act(self.bn_h(x_h))
+        x_l = self.act(self.bn_l(x_l)) if x_l is not None else None
+        return x_h, x_l
+
+def norm(norm_type, nc):
+    norm_type = norm_type.lower()
+    if norm_type == 'batch':
+        layer = nn.BatchNorm2d(nc, affine=True)
+    elif norm_type == 'instance':
+        layer = nn.InstanceNorm2d(nc, affine=False)
+    else:
+        raise NotImplementedError('normalization layer [{:s}] is not found'.format(norm_type))
+    return layer
+
+
+def pad(pad_type, padding):
+    pad_type = pad_type.lower()
+    if padding == 0:
+        return None
+    if pad_type == 'reflect':
+        layer = nn.ReflectionPad2d(padding)
+    elif pad_type == 'replicate':
+        layer = nn.ReplicationPad2d(padding)
+    else:
+        raise NotImplementedError('padding layer [{:s}] is not implemented'.format(pad_type))
+    return layer
+
+
+def get_valid_padding(kernel_size, dilation):
+    kernel_size = kernel_size + (kernel_size - 1) * (dilation - 1)
+    padding = (kernel_size - 1) // 2
+    return padding
+
+
+def conv_block(in_nc, out_nc, kernel_size, stride=1, dilation=1, groups=1, bias=True,
+               pad_type='zero', norm_type=None, act_type='relu'):
+    padding = get_valid_padding(kernel_size, dilation)
+    p = pad(pad_type, padding) if pad_type and pad_type != 'zero' else None
+    padding = padding if pad_type == 'zero' else 0
+
+    c = nn.Conv2d(in_nc, out_nc, kernel_size=kernel_size, stride=stride, padding=padding,
+                  dilation=dilation, bias=bias, groups=groups)
+    a = activation(act_type) if act_type else None
+    n = norm(norm_type, out_nc) if norm_type else None
+    return sequential(p, c, n, a)
+
+
+def activation(act_type, inplace=True, neg_slope=0.05, n_prelu=1):
+    act_type = act_type.lower()
+    if act_type == 'relu':
+        layer = nn.ReLU(inplace)
+    elif act_type == 'lrelu':
+        layer = nn.LeakyReLU(neg_slope, inplace)
+    elif act_type == 'prelu':
+        layer = nn.PReLU(num_parameters=n_prelu, init=neg_slope)
+    else:
+        raise NotImplementedError('activation layer [{:s}] is not found'.format(act_type))
+    return layer
+
+
+class ShortcutBlock(nn.Module):
+    def __init__(self, submodule):
+        super(ShortcutBlock, self).__init__()
+        self.sub = submodule
+
+    def forward(self, x):
+        output = x + self.sub(x)
+        return output
+
+def mean_channels(F):
+    assert(F.dim() == 4)
+    spatial_sum = F.sum(3, keepdim=True).sum(2, keepdim=True)
+    return spatial_sum / (F.size(2) * F.size(3))
+
+def stdv_channels(F):
+    assert(F.dim() == 4)
+    F_mean = mean_channels(F)
+    F_variance = (F - F_mean).pow(2).sum(3, keepdim=True).sum(2, keepdim=True) / (F.size(2) * F.size(3))
+    return F_variance.pow(0.5)
+
+def sequential(*args):
+    if len(args) == 1:
+        if isinstance(args[0], OrderedDict):
+            raise NotImplementedError('sequential does not support OrderedDict input.')
+        return args[0]
+    modules = []
+    for module in args:
+        if isinstance(module, nn.Sequential):
+            for submodule in module.children():
+                modules.append(submodule)
+        elif isinstance(module, nn.Module):
+            modules.append(module)
+    return nn.Sequential(*modules)
+
+class ESA(nn.Module):
+    def __init__(self, n_feats, conv):
+        super(ESA, self).__init__()
+        f = n_feats // 4
+        self.conv1 = conv(n_feats, f, kernel_size=1)
+        self.conv_f = conv(f, f, kernel_size=1)
+        self.conv_max = conv(f, f, kernel_size=3, padding=1)
+        self.conv2 = conv(f, f, kernel_size=3, stride=2, padding=0)
+        self.conv3 = conv(f, f, kernel_size=3, padding=1)
+        self.conv3_ = conv(f, f, kernel_size=3, padding=1)
+        self.conv4 = conv(f, n_feats, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        c1_ = (self.conv1(x))
+        c1 = self.conv2(c1_)
+        v_max = F.max_pool2d(c1, kernel_size=7, stride=3)
+        v_range = self.relu(self.conv_max(v_max))
+        c3 = self.relu(self.conv3(v_range))
+        c3 = self.conv3_(c3)
+        c3 = F.interpolate(c3, (x.size(2), x.size(3)), mode='bilinear', align_corners=False) 
+        cf = self.conv_f(c1_)
+        c4 = self.conv4(c3+cf)
+        m = self.sigmoid(c4)
+        
+        return x * m
+
+
+class RFDB(nn.Module):
+    def __init__(self, in_channels, distillation_rate=0.25):
+        super(RFDB, self).__init__()
+        self.dc = self.distilled_channels = in_channels//2
+        self.rc = self.remaining_channels = in_channels
+        #self.c1_d = conv_layer(in_channels, self.dc, 1)
+	self.c1_d = OctaveConv(in_channels*2 , in_channels*2 , 1)
+        
+	#self.c1_r = conv_layer(in_channels, self.rc, 3)
+        #self.c2_d = conv_layer(self.remaining_channels, self.dc, 1)
+        self.c2_d = OctaveConv(in_channels*2 , in_channels*2 , 1)
+	
+	#self.c2_r = conv_layer(self.remaining_channels, self.rc, 3)
+        self.c3_d = conv_layer(self.remaining_channels, self.dc, 1)
+        self.c3_d = OctaveConv(in_channels*2 , in_channels*2 , 1)
+	#self.c3_r = conv_layer(self.remaining_channels, self.rc, 3)
+        #self.c4 = conv_layer(self.remaining_channels, self.remaining_channels, 3)
+        self.act = activation('lrelu', neg_slope=0.05)
+        self.c5 = conv_layer(self.rc*4, in_channels, 1)
+        self.esa = ESA(in_channels, nn.Conv2d)
+
+    def forward(self, input):
+        #distilled_c1 = self.act(self.c1_d(input))
+	distilled_c1, r_c1 = self.c1_d(input)
+        distilled_c1 = self.act(distilled_c1)
+	#r_c1 = (self.c1_r(input))
+        r_c1 = self.act(r_c1+input)
+
+        #distilled_c2 = self.act(self.c2_d(r_c1))
+        distilled_c2, r_c2 = self.c2_d(r_c1)
+	distilled_c2 = self.act(distilled_c2)
+	#r_c2 = (self.c2_r(r_c1))
+        r_c2 = self.act(r_c2+r_c1)
+
+        #distilled_c3 = self.act(self.c3_d(r_c2))
+        distilled_c2, r_c2 = self.c2_d(r_c2)
+	distilled_c3 = self.act(distilled_c3)
+	#r_c3 = (self.c3_r(r_c2))
+        r_c3 = self.act(r_c3+r_c2)
+
+        r_c4 = self.act(self.c4(r_c3))
+
+        out = torch.cat([distilled_c1, distilled_c2, distilled_c3, r_c4], dim=1)
+        out_fused = self.esa(self.c5(out)) 
+
+        return out_fused
+
+
+
+def pixelshuffle_block(in_channels, out_channels, upscale_factor=2, kernel_size=3, stride=1):
+    conv = conv_layer(in_channels, out_channels * (upscale_factor ** 2), kernel_size, stride)
+    pixel_shuffle = nn.PixelShuffle(upscale_factor)
+    return sequential(conv, pixel_shuffle)
